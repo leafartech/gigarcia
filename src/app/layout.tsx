3bb68ae04ac99490540b1b00430abdb7adab75ff
leafartech <119262053+leafartech@users.noreply.github.com>
2023-08-25 09:08:45 -0300

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Carrossel de outro - Gigarcia ',
+  description: 'Gigarcia',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <head>
+        <title>Carrossel de ouro</title>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-PPJ4DXS');
+              `,
+          }}
+        ></script>
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
